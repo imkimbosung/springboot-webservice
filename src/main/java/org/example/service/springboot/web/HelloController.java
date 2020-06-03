eb58fd581503223905e0276a19425f5609542cb0
bosungkim <ghalsru@naver.com>
2020-06-04 00:15:35 +0900

    add testcode 작성

---
+++ src/main/java/org/example/service/springboot/web/HelloController.java
@@ -0,0 +1,13 @@
+package org.example.service.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어줌
+public class HelloController {
+
+    @GetMapping("/hello")  // HTTP Method인 GET 요청을 받을 수 있게 해줌
+   public String hello(){
+        return "hello";
+    }
+}
