93ed434dc773134a8beced53bc06bf5816929edf
bosungkim <ghalsru@naver.com>
2020-06-06 23:01:45 +0900

    Spring Data Jpa 적용

---
+++ src/main/java/org/example/service/springboot/domain/posts/Posts.java
@@ -0,0 +1,36 @@
+package org.example.service.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity   // 테이블과 링크될 클스라는 표기
+public class Posts {
+
+    @Id // 해당 필드의 PK값을 표시
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // 스프링 생성 규칙
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
